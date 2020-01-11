66b213830b6856a6387c844f65954e6239be48e7
Ryan Godfrey <ryan_crothers@hotmail.com>
2020-01-11 14:51:32 -0500

    Test

--- MurderMystery.py
+++ MurderMystery.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import json
+
+with open('Murder on the 2nd Floor/Murder-on-the-2nd-Floor-Raw-Data.json', 'r') as f:
+    murd_dict = json.load(f)
+
+people = {}
+
+def event_dictionary(name):
+    event_dic = {}
+    count = 0
+    for key in murd_dict:
+        if murd_dict[key]['guest-id'] == name:
+            event_dic[key] = murd_dict[key]
+            count += 1
+    return event_dic
+
+for key in murd_dict:
+    people[murd_dict[key]['guest-id']] = ''
+
+for person in people:
+    print(person + ":")
+    print(event_dictionary(person))
+    print("\n")
