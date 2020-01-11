a18778e1dd2548dccdd7c13158dba7e91c5c7d8a
Mariam05 <tomariam@gmail.com>
2020-01-11 15:30:41 -0500

    Adding buttons to main page

--- MurderMystery.py
+++ MurderMystery.py
@@ -1,8 +1,27 @@
 import json
 import datetime
+import tkinter as tk
 
 print("Hello World!")
+
+names = {"Jason", "Selina", "Mariam"}
+
 
 def convertEpochtoUTC(epoch):
     return datetime.datetime.utcfromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S');
 
+
+r = tk.Tk()
+
+r.title("Martello Murder Mystery")
+
+thisdict = {"brand":"Frod", "model": "Mustang", "year": 1954}
+
+for x in names:
+    button = tk.Button(r, text=x, width=50, command=r.destroy )    
+    button.pack()
+    
+def displayData():
+    print("Hello");
+    
+r.mainloop() 
