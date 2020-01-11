427b7f74198b0200402c72cf08341e2a1a5e59de
Ryan Godfrey <ryan_crothers@hotmail.com>
2020-01-11 15:07:05 -0500

    Test

--- MurderMystery.py
+++ MurderMystery.py
@@ -1,27 +1,31 @@
 import json
 import datetime
 
-people = {}
-
 with open('Murder on the 2nd Floor/Murder-on-the-2nd-Floor-Raw-Data.json', 'r') as f:
     murd_dict = json.load(f)
 
-def convertEpochtoUTC(epoch):
-    return datetime.datetime.utcfromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S');
-
-def event_dictionary(name):
-    event_dic = {}
-    count = 0
-    for key in murd_dict:
-        if murd_dict[key]['guest-id'] == name:
-            event_dic[key] = murd_dict[key]
-            count += 1
-    return event_dic
+people = {}
 
 for key in murd_dict:
     people[murd_dict[key]['guest-id']] = ''
 
+
+def convert_epoch_to_utc(epoch):
+    return datetime.datetime.utcfromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def event_dictionary(name):
+    event_dic = {}
+    count = 0
+    for event_key in murd_dict:
+        if murd_dict[event_key]['guest-id'] == name:
+            event_dic[event_key] = murd_dict[event_key]
+            count += 1
+    return event_dic
+
+
 for person in people:
     print(person + ":")
-    print(event_dictionary(person))
+    people[person] = event_dictionary(person)
+    print(people[person])
     print("\n")
