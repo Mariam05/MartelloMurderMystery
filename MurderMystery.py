43e141800d0283c269d4d298ccdd911ba19aeedd
Ryan Godfrey <ryan_crothers@hotmail.com>
2020-01-11 15:45:07 -0500

    Test

--- MurderMystery.py
+++ MurderMystery.py
@@ -5,6 +5,30 @@ with open('Murder on the 2nd Floor/Murder-on-the-2nd-Floor-Raw-Data.json', 'r') 
     murd_dict = json.load(f)
 
 people = {}
+rooms = {'100': 'Front Lobby',
+         '101': 'Reception Closet',
+         '105': 'Dining Hall',
+         '110': 'Conference Room',
+         '130': 'Kitchen',
+         '151': 'Gym',
+         '152': 'Mens Washroom',
+         '154': 'Womans Washroom',
+         '155': 'Pool',
+         '156': 'Laundry Room',
+         '156B': 'Stroage Room',
+         '200': 'Hall',
+         '210': 'Executive Suite (Murder)',
+         '220': 'Executive Suite',
+         '231': 'Comfort Room 1',
+         '232': 'Comfort Room 2',
+         '233': 'Comfort Room 3',
+         '234': 'Ice and Vending Machines',
+         '235': 'Comfort Room 4',
+         '236': 'Comfort Room 5',
+         '241': 'Junior Suite 1',
+         '244': 'Junior Suite 2',
+         '247': 'Junior Suite 3',
+         '248': 'Junior Suite 4'}
 
 for key in murd_dict:
     people[murd_dict[key]['guest-id']] = ''
@@ -26,6 +50,3 @@ def event_dictionary(name):
 
 for person in people:
     people[person] = event_dictionary(person)
-
-
-
