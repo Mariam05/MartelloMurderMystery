bc1575f0f627232be0009c89cce788e788e2adff
Mariam05 <tomariam@gmail.com>
2020-01-11 21:14:25 -0500

    Can filter by person

--- MurderMystery.py
+++ MurderMystery.py
@@ -5,6 +5,7 @@ from tkinter import *
 from tkinter import Toplevel
 from faulthandler import disable
 from _overlapped import NULL
+from http import client
 
 with open('Murder on the 2nd Floor/Murder-on-the-2nd-Floor-Raw-Data.json', 'r') as f:
     murd_dict = json.load(f)
@@ -47,6 +48,7 @@ rooms = {'100': 'Front Lobby',
 
 
 class Person:
+
     def __init__(self, name, rooms):
         self.room_dict = rooms
         self.name = name
@@ -67,6 +69,7 @@ class Person:
 
 # noinspection DuplicatedCode
 class Room:
+
     def __init__(self, number):
         self.people = []
         self.prev_people = []
@@ -90,6 +93,7 @@ class Room:
 
 
 class Floor:
+
     def __init__(self, number):
         self.number = number
         self.people = []
@@ -142,6 +146,7 @@ for person in people_dict:  # create dictionaries for ech person, containing the
 for person in people_dict:  # initialize array of Person objects with initial locations
     people_arr.append(Person(person, people_dict[person]))
 
+
 def setUpGUI():
     r = tk.Tk()
     frame = tk.Frame(r)
@@ -158,12 +163,11 @@ def setUpGUI():
         button.pack()
         print(people[person]);
         
-    for x in people:
+    for x in people_dict:
         button = tk.Button(frame, text=x, width=50, command=lambda x=x: displayData(x))    
         button.pack()
             
     r.mainloop()
-
 
 """
 Second format where there's a menu bar that the user can use to filter .. 
@@ -172,34 +176,69 @@ I still haven't made it so that the data is displayed when one is clicked though
 def setUpGUI2():
     root = tk.Tk()
     
-    filteredPerson = ""
+    filteredPeople = []
     filteredRoom = ""
     
     textBox = tk.Text(root, height=50, width=100)
     textBox.pack()
     
-    textBox.config(state="disabled")
+    textBox.config(state="disabled")  # Don't allow the user to edit the textbox
     
     menubar = Menu(root)
     personMenu = Menu(menubar)
+    roomMenu = Menu(menubar)
     
+    # Display information about the person selected. TODO: If they click on it again, remove it from filtered
     def personSelected(person):
-        filteredPerson = person
-        textBox.config(state="normal")
-        textBox.delete(1.0, END)
-        textBox.insert(tk.INSERT, "FILTERED BY: " + filteredPerson + "\n")
-        textBox.insert(tk.INSERT, person + "\n")  
-        textBox.config(state="disabled")
+        if person in filteredPeople:
+            filteredPeople.remove(person)
+        else:      
+            filteredPeople.append(person)  # Add the person to the filtered ppl data
         
-    for person in people:
+        
+        textBox.config(state="normal") # Enable textbox editing so that we can write to it
+        textBox.delete(1.0, END) # Clear the textbox 
+
+        #This is a dictionary of all the enteries in murd_dict with that person in them
+        filteredpeople_dict = [{i:j for (i,j) in murd_dict.items() if (j['guest-id'] in filteredPeople)}]
+        
+        # Let the user know what they're filtering by
+        textBox.insert(tk.INSERT, "FILTERED BY: \n")
+        textBox.insert(tk.INSERT, "PEOPLE: ")
+        for p in filteredPeople:
+            textBox.insert(tk.INSERT, p + " ")
+            
+        textBox.insert(tk.INSERT, "\n\n\n ")
+        textBox.insert(tk.INSERT, ("{:<15} {:<25} {:<20}".format('Time','Room','Person')+"\n"))
+        
+            
+        for element in filteredpeople_dict:
+            for x, y in element.items():
+                textBox.insert(tk.INSERT, ("{:<15} {:<25}{:<20}".format(x, rooms[y['device-id']], y['guest-id'])+"\n"))
+                
+        
+            
+          
+        textBox.config(state="disabled")  # disable it so that it can't be changed again
+        
+    # Create the menu bar for the people so that the user can select which user to filter by
+    for person in people_dict: 
         personMenu.add_command(label=person, command=lambda person=person: personSelected(person))
-        
     menubar.add_cascade(label="People", menu=personMenu)
+    
+    # Create the menu bar for the rooms so that a user can select which room to filter by
+    for room in rooms:
+        roomMenu.add_command(label=room, command=lambda person=person: personSelected(person))       
+    menubar.add_cascade(label="Room", menu=roomMenu)
     
     # display the menu
     root.config(menu=menubar)
         
     root.mainloop()
+
     
-    
-setUpGUI2();
+# print("Murder Dict: " , murd_dict)
+# for time, info in murd_dict.items():
+#     print (info['guest-id'])  
+setUpGUI2()
+
